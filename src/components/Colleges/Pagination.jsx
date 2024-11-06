812cf68c651f0e065711fd36ec868f28915abcc0
sammy429b <samarthnbahirgonde49@gmail.com>
2024-11-06 17:36:28 +0530

    College pages all components added

---
+++ src/components/Colleges/Pagination.jsx
@@ -0,0 +1,98 @@
+"use client"
+import React from "react";
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const pageNumbers = [];
+  const maxPageNumbersToShow = 5; // Number of page numbers to display at once
+  let startPage, endPage;
+
+  if (totalPages <= maxPageNumbersToShow) {
+    // If total pages are less than or equal to maxPageNumbersToShow, show all pages
+    startPage = 1;
+    endPage = totalPages;
+  } else {
+    // Determine start and end page based on the current page
+    const middlePage = Math.ceil(maxPageNumbersToShow / 2);
+    if (currentPage <= middlePage) {
+      startPage = 1;
+      endPage = maxPageNumbersToShow;
+    } else if (currentPage + middlePage - 1 >= totalPages) {
+      startPage = totalPages - maxPageNumbersToShow + 1;
+      endPage = totalPages;
+    } else {
+      startPage = currentPage - middlePage + 1;
+      endPage = currentPage + middlePage - 1;
+    }
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <ol className="flex justify-center gap-2 text-sm font-medium mb-5">
+      <li>
+        <button
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={`inline-flex w-10 h-10 items-center justify-center rounded border border-gray-200 bg-white text-gray-900 ${
+            currentPage === 1 ? "cursor-not-allowed opacity-50" : ""
+          }`}
+        >
+          <span className="sr-only">Prev Page</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4" // Increased size of the arrow icon
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </li>
+      {pageNumbers.map((number) => (
+        <li key={number}>
+          <button
+            onClick={() => onPageChange(number)}
+            className={`block w-10 h-10 rounded border text-center leading-10 ${
+              currentPage === number
+                ? "border-blue-600 bg-blue-600 text-white"
+                : "border-gray-200 bg-white text-gray-900"
+            }`}
+          >
+            {number}
+          </button>
+        </li>
+      ))}
+      <li>
+        <button
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className={`inline-flex w-10 h-10 items-center justify-center rounded border border-gray-200 bg-white text-gray-900 ${
+            currentPage === totalPages ? "cursor-not-allowed opacity-50" : ""
+          }`}
+        >
+          <span className="sr-only">Next Page</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4" // Increased size of the arrow icon
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </li>
+    </ol>
+  );
+};
+
+export default Pagination;
---
+++ src/components/Colleges/Ranking.jsx
@@ -0,0 +1,504 @@
+"use client"
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Slider from "@mui/material/Slider";
+// import "../../styles/Colleges/Ranking.css"; 
+
+function Ranking({
+  filterNaac,
+  handleNaacFilter,
+  sortOrder,
+  handleSortChange,
+  onStateChange,
+  onNbaFilter,
+  openFilters,
+  setOpenFilters
+  
+}) {
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [nirfRanking, setNirfRanking] = useState([0, 200]);
+  const [nbaAccreditation, setNbaAccreditation] = useState("");
+
+  const handleStateChange = (event) => {
+    setSelectedState(event.target.value);
+    onStateChange(event.target.value);
+  };
+
+  const handleSliderChange = (event, newValue) => {
+    setNirfRanking(newValue);
+    handleSortChange(newValue);
+  };
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+    onStateChange(event.target.value);
+  };
+
+  const handleNbaChange = (event) => {
+    setNbaAccreditation(event.target.value);
+    onNbaFilter(event.target.value);
+  };
+
+  const naacOptions = [
+    "NAAC A",
+    "NAAC A+",
+    "NAAC A++",
+    "NAAC B",
+    "NAAC B+",
+    "NAAC B++",
+    "NAAC C",
+    "NAAC D",
+  ];
+  const stateOptions = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Goa",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttarakhand",
+    "Uttar Pradesh",
+    "West Bengal",
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Delhi",
+    "Lakshadweep",
+    "Puducherry",
+  ];
+  const cityOptions = [
+    "Kurukshetra",
+    "Mumbai",
+    "Chennai",
+    "Delhi",
+    "Hyderabad",
+    "Bengaluru",
+    "Jaipur",
+    "Ahemdabad",
+    "Kolkata",
+    "Indore",
+  ];
+
+  const nbaOptions = ["Accredited"];
+
+  const formControlLabelStyle = {
+    display: "flex",
+    justifyContent: "flex-start",
+  };
+
+  return (
+    <Grid container direction="column" 
+    className="outer-box bg-white border border-[rgb(213,211,211)] text-[rgb(17,17,27)] w-[200px] p-[10px] rounded-[10px]">
+      <Grid item className="Inner-Topic flex justify-between">
+        <h5 className="Apply-Filter  font-bold mb-1 ">Apply Filter</h5>
+        {openFilters !== null &&
+        <button 
+        className="md:hidden font-bold bg-blue-500 text-white px-4 py-2 rounded-lg"
+        onClick={() => {
+          if(openFilters){
+            setOpenFilters(!openFilters)
+          }
+        }}
+        >
+          X
+        </button>}
+      </Grid>
+
+      <Grid item className="Inner-Topic">
+        <hr />
+        <h4 className="font-bold mb-2 sub-topic">Select City</h4>
+        <hr/>
+      </Grid>
+      <div className="scrollable-container max-h-[300px] overflow-y-auto">
+        {cityOptions.map((city) => (
+          <Grid item key={city}>
+            <FormControlLabel className="Check-Box p-0 m-0 h-[25px]"
+              control={
+                <Checkbox
+                 
+                  checked={selectedCity === city}
+                  onChange={handleCityChange}
+                  value={city}
+                />
+              }
+              label={city}
+              style={formControlLabelStyle}
+            />
+          </Grid>
+        ))}
+      </div>
+
+      <Grid item className="Inner-Topic">
+        <hr />
+        <h4 className="font-bold mb-1 sub-topic text-left">Top State</h4>
+        <hr/>
+      </Grid>
+      <div className="scrollable-container max-h-[300px] overflow-y-auto">
+        {stateOptions.map((state) => (
+          <Grid className="" item key={state}>
+            <FormControlLabel className="Check-Box p-0 m-0 h-[25px]"
+              control={
+                <Checkbox
+                  checked={selectedState === state}
+                  onChange={handleStateChange}
+                  value={state}
+                />
+              }
+              label={state}
+              style={formControlLabelStyle}
+            />
+          </Grid>
+        ))}
+      </div>
+
+      <Grid>
+        <hr />
+        <h2 className="font-bold mb-2 sub-topic">NAAC Rating</h2>
+        <hr/>
+      </Grid>
+      <div className="scrollable-container max-h-[300px] overflow-y-auto">
+        {naacOptions.map((option) => (
+          <Grid item key={option}>
+            <FormControlLabel className="Check-Box p-0 m-0 h-[25px]"
+
+              control={
+                <Checkbox
+                  checked={filterNaac === option}
+                  onChange={ handleNaacFilter}
+                  value={option}
+                />
+              }
+              label={option}
+              style={formControlLabelStyle}
+            />
+          </Grid>
+        ))}
+      </div>
+
+      <Grid item className="Inner-Topic">
+        <hr />
+        <h2 className="font-bold mb-2 sub-topic">NIRF Ranking</h2>
+        <hr/>
+      </Grid>
+      <Grid item>
+        <Slider
+          value={nirfRanking}
+          onChange={handleSliderChange}
+          valueLabelDisplay="auto"
+          min={0}
+          max={200}
+          step={1}
+        />
+      </Grid>
+
+      <Grid item className="Inner-Topic">
+        <hr />
+        <h2 className="font-bold mb-2 sub-topic">NBA Accreditation</h2>
+        <hr/>
+      </Grid>
+      <div className="scrollable-container max-h-[300px] overflow-y-auto">
+        {nbaOptions.map((option) => (
+          <Grid item key={option}>
+            <FormControlLabel className="Check-Box p-0"
+
+              control={
+                <Checkbox
+                  checked={nbaAccreditation === option}
+                  onChange={handleNbaChange}
+                  value={option}
+                />
+              }
+              label={option}
+              style={formControlLabelStyle}
+            />
+          </Grid>
+        ))}
+      </div>
+      <button
+        className="clear-filter-btn font-bold mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg"
+        onClick={() => {
+          setSelectedState("");
+          setSelectedCity("");
+          setNirfRanking([0, 200]);
+          setNbaAccreditation("");
+          handleNaacFilter({target:{value:""}});
+          onStateChange("");
+          onNbaFilter("");
+        }}
+      >Clear Filters</button>
+    </Grid>
+  );
+}
+
+export default Ranking;
+
+
+// import React, { useState } from "react";
+// import Grid from "@mui/material/Grid";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+// import Slider from "@mui/material/Slider";
+// import "./Ranking.css"; // Import the CSS file
+
+// function Ranking({
+//   filterNaac,
+//   handleNaacFilter,
+//   sortOrder,
+//   handleSortChange,
+//   onStateChange,
+//   onNbaFilter,
+// }) {
+//   const [selectedStates, setSelectedStates] = useState([]);
+//   const [selectedCities, setSelectedCities] = useState([]);
+//   const [nirfRanking, setNirfRanking] = useState([0, 200]);
+//   const [nbaAccreditations, setNbaAccreditations] = useState([]);
+
+//   const handleStateChange = (event) => {
+//     const value = event.target.value;
+//     setSelectedStates((prevSelected) =>
+//       prevSelected.includes(value)
+//         ? prevSelected.filter((state) => state !== value)
+//         : [...prevSelected, value]
+//     );
+//     onStateChange(value);
+//   };
+
+//   const handleCityChange = (event) => {
+//     const value = event.target.value;
+//     setSelectedCities((prevSelected) =>
+//       prevSelected.includes(value)
+//         ? prevSelected.filter((city) => city !== value)
+//         : [...prevSelected, value]
+//     );
+//     onStateChange(value);
+//   };
+
+//   const handleNbaChange = (event) => {
+//     const value = event.target.value;
+//     setNbaAccreditations((prevSelected) =>
+//       prevSelected.includes(value)
+//         ? prevSelected.filter((nba) => nba !== value)
+//         : [...prevSelected, value]
+//     );
+//     onNbaFilter(value);
+//   };
+
+//   const handleSliderChange = (event, newValue) => {
+//     setNirfRanking(newValue);
+//     handleSortChange(newValue);
+//   };
+
+//   const naacOptions = [
+//     "NAAC A",
+//     "NAAC A+",
+//     "NAAC A++",
+//     "NAAC B",
+//     "NAAC B+",
+//     "NAAC B++",
+//     "NAAC C",
+//     "NAAC D",
+//   ];
+//   const stateOptions = [
+//     "Andhra Pradesh",
+//     "Arunachal Pradesh",
+//     "Assam",
+//     "Bihar",
+//     "Chhattisgarh",
+//     "Gujarat",
+//     "Haryana",
+//     "Himachal Pradesh",
+//     "Jammu and Kashmir",
+//     "Goa",
+//     "Jharkhand",
+//     "Karnataka",
+//     "Kerala",
+//     "Madhya Pradesh",
+//     "Maharashtra",
+//     "Manipur",
+//     "Meghalaya",
+//     "Mizoram",
+//     "Nagaland",
+//     "Odisha",
+//     "Punjab",
+//     "Rajasthan",
+//     "Sikkim",
+//     "Tamil Nadu",
+//     "Telangana",
+//     "Tripura",
+//     "Uttarakhand",
+//     "Uttar Pradesh",
+//     "West Bengal",
+//     "Andaman and Nicobar Islands",
+//     "Chandigarh",
+//     "Dadra and Nagar Haveli",
+//     "Daman and Diu",
+//     "Delhi",
+//     "Lakshadweep",
+//     "Puducherry",
+//   ];
+//   const cityOptions = [
+//     "Kurukshetra",
+//     "Mumbai",
+//     "Chennai",
+//     "Delhi",
+//     "Hyderabad",
+//     "Bengaluru",
+//     "Jaipur",
+//     "Ahemdabad",
+//     "Kolkata",
+//     "Indore",
+//   ];
+
+//   const nbaOptions = ["Accredited"];
+
+//   const formControlLabelStyle = {
+//     display: "flex",
+//     justifyContent: "flex-start",
+//   };
+
+//   return (
+//     <Grid container direction="column" className="outer-box">
+//       <Grid item className="Inner-Topic">
+//         <h5 className="Apply-Filter font-bold mb-1">Apply Filter</h5>
+//       </Grid>
+
+//       <Grid item className="Inner-Topic">
+//         <hr />
+//         <h4 className="font-bold mb-2 sub-topic">Select City</h4>
+//         <hr />
+//       </Grid>
+//       <div className="scrollable-container">
+//         {cityOptions.map((city) => (
+//           <Grid item key={city}>
+//             <FormControlLabel
+//               className="Check-Box p-0"
+//               control={
+//                 <Checkbox
+//                   checked={selectedCities.includes(city)}
+//                   onChange={handleCityChange}
+//                   value={city}
+//                 />
+//               }
+//               label={city}
+//               style={formControlLabelStyle}
+//             />
+//           </Grid>
+//         ))}
+//       </div>
+
+//       <Grid item className="Inner-Topic">
+//         <hr />
+//         <h4 className="font-bold mb-1 sub-topic">Top State</h4>
+//         <hr />
+//       </Grid>
+//       <div className="scrollable-container">
+//         {stateOptions.map((state) => (
+//           <Grid className="mb-0" item key={state}>
+//             <FormControlLabel
+//               className="Check-Box p-0"
+//               control={
+//                 <Checkbox
+//                   checked={selectedStates.includes(state)}
+//                   onChange={handleStateChange}
+//                   value={state}
+//                 />
+//               }
+//               label={state}
+//               style={formControlLabelStyle}
+//             />
+//           </Grid>
+//         ))}
+//       </div>
+
+//       <Grid>
+//         <hr />
+//         <h2 className="font-bold mb-2 sub-topic">NAAC Rating</h2>
+//         <hr />
+//       </Grid>
+//       <div className="scrollable-container">
+//         {naacOptions.map((option) => (
+//           <Grid item key={option}>
+//             <FormControlLabel
+//               className="Check-Box p-0"
+//               control={
+//                 <Checkbox
+//                   checked={filterNaac === option}
+//                   onChange={handleNaacFilter}
+//                   value={option}
+//                 />
+//               }
+//               label={option}
+//               style={formControlLabelStyle}
+//             />
+//           </Grid>
+//         ))}
+//       </div>
+
+//       <Grid item className="Inner-Topic">
+//         <hr />
+//         <h2 className="font-bold mb-2 sub-topic">NIRF Ranking</h2>
+//         <hr />
+//       </Grid>
+//       <Grid item>
+//         <Slider
+//           value={nirfRanking}
+//           onChange={handleSliderChange}
+//           valueLabelDisplay="auto"
+//           min={0}
+//           max={200}
+//           step={1}
+//         />
+//       </Grid>
+
+//       <Grid item className="Inner-Topic">
+//         <hr />
+//         <h2 className="font-bold mb-2 sub-topic">NBA Accreditation</h2>
+//         <hr />
+//       </Grid>
+//       <div className="scrollable-container">
+//         {nbaOptions.map((option) => (
+//           <Grid item key={option}>
+//             <FormControlLabel
+//               className="Check-Box p-0"
+//               control={
+//                 <Checkbox
+//                   checked={nbaAccreditations.includes(option)}
+//                   onChange={handleNbaChange}
+//                   value={option}
+//                 />
+//               }
+//               label={option}
+//               style={formControlLabelStyle}
+//             />
+//           </Grid>
+//         ))}
+//       </div>
+//     </Grid>
+//   );
+// }
+
+// export default Ranking;
