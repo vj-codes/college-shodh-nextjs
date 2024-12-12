c137dc174014859981c6674ed89d13b0a5f5c701
sammy429b <samarthnbahirgonde49@gmail.com>
2024-12-13 00:56:20 +0530

    Added SearchContext for centralized management of search term state

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/header/Header';
 import './globals.css';
 import Footer from '@/components/footer/Footer';
+import { SearchProvider } from '@/context/SearchContext';
 
 export const metadata = {
   title: 'Next.js',
@@ -11,9 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header/>
-        {children}
-        <Footer/>
+        <SearchProvider>
+          <Header />
+          {children}
+          <Footer />
+        </SearchProvider>
       </body>
     </html>
   )
