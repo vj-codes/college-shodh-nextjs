01f607729a4c7b3f64e3c40d463a9e7d97b41de3
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-01-24 14:05:02 +0530

    UI: Home page cities and courses

--- src/components/home/Programs.jsx
+++ src/components/home/Programs.jsx
@@ -1,82 +1,107 @@
-"use client"
-import React, { useState } from "react";
-import { CollegeContext } from "@/context/CollegContext";
-import MyModal from "../Modals/Modal";
-import Link from "next/link";
-import { useContext } from "react";
+"use client";
+import React, { useState, useEffect, useContext } from "react";
+import { IoArrowForwardOutline } from "react-icons/io5";
+import { SearchContext } from "@/context/SearchContext";
+import { redirect } from "next/navigation";
+import Image from "next/image";
 
+// Import images
+import barch_image from "../../assets/barch-min.jpg";
+import bpharma from "../../assets/bpharma-min.jpg";
+import bca from "../../assets/bca-min.jpg";
+import bebtech from "../../assets/bebtech-min.jpg";
+import bsc from "../../assets/bsc-min.jpg";
+
+// Image and option arrays
+const BackgroundImages = [barch_image, bpharma, bca, bebtech, bsc];
 
 const Options = [
-  { text: "B.Arch", img: "./programs/barch.png",course: "Architecture", link: "#" },
-  { text: "B.Pharm", img: "./programs/bphram.png", course: "Pharmacy", link: "#" },
-  { text: "BCA", img: "./programs/bca.png", course: "BCA", link: "#", link: "#" },
-  { text: "BE/B.Tech", img: "./programs/be-btech.png", course: "Engineering", link: "#" },
+  {
+    text: "B.Arch",
+    img: "./programs/barch.png",
+    course: "Architecture",
+    link: "#",
+  },
+  {
+    text: "B.Pharm",
+    img: "./programs/bphram.png",
+    course: "Pharmacy",
+    link: "#",
+  },
+  {
+    text: "BCA",
+    img: "./programs/bca.png",
+    course: "BCA",
+    link: "#",
+    link: "#",
+  },
+  {
+    text: "BE/B.Tech",
+    img: "./programs/be-btech.png",
+    course: "Engineering",
+    link: "#",
+  },
   { text: "B.Sc", img: "./programs/bsc.png", course: "Science", link: "#" },
 ];
 
-const Programs = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [selectedType, setSelectedType] = useState("BSc"); // Default to BSc
-  const {selectedCity, setSelectedCity, selectedCourse, setSelectedCourse} = useContext(CollegeContext)
+export default function Home() {
+  const [redirectUrl, setRedirectUrl] = useState(null);
+  const [search, setSearch] = useState("");
+  const { setSearchTerm } = useContext(SearchContext);
+  const [bgIndex, setBgIndex] = useState(0);
+  const [optionIndex, setOptionIndex] = useState(0);
 
-  const handleCityClick = (cityName) => {
-    setSelectedCourse(cityName);
-  };
-  const closeModal = (e) => {
-    setShowModal(false);    
-    if (e.name){
-      redirectCourse(e.name);
-    }    
-    e.stopPropagation();
-  }
-
-
-  const openModal = (type) => {
-    setShowModal(true);
-    setSelectedType(type);    
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
   };
 
-  const redirectCourse = (course) => {
-    window.location.href = `/colleges?course=${course}`
-  }
+  const handleSearchClick = (e) => {
+    e.preventDefault();
+    setSearchTerm(search);
+    setRedirectUrl("/colleges");
+  };
 
+  useEffect(() => {
+    if (redirectUrl) {
+      redirect(redirectUrl); // Redirect on state change
+    }
+  }, [redirectUrl]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBgIndex((prevIndex) => (prevIndex + 1) % BackgroundImages.length);
+      setOptionIndex((prevIndex) => (prevIndex + 1) % Options.length);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
-    <div className="text-center pb-8">
-      <div>
-        <p className="text-2xl font-system-ui p-4 mt-4 text-gray-800 bg-neutral-200">
-          Discover Bachelor's Program in Science, Technology, Engineering and
-          Mathematics
-        </p>
-      </div>
+    <div>
+   
 
-      <div className="text-left ml-32 mt-8">
-        <p className="text-2xl font-semibold">Explore Programs</p>
-      </div>
-
-      <div className="mt-5 mb-5 flex flex-wrap justify-center gap-4">
-        {Options.map((option, index) => (
-          <Link href={`/colleges`} key={index}>
-            <button
-              onClick={() => handleCityClick(option.course)}
-              className="h-28 w-52 bg-gray-200 border border-black rounded-lg flex flex-col items-center justify-center hover:shadow-xl hover:bg-slate-300 transition-all"
+      {/* Programs Section */}
+      <div className="text-center mt-8">
+        <div className="text-lg sm:text-xl lg:text-2xl font-semibold mb-4">
+          Programs
+        </div>
+        <div className="flex flex-wrap gap-4 justify-center mx-4">
+          {Options.map((option, index) => (
+            <div
+              key={index}
+              className="h-24 w-36 sm:h-28 sm:w-52 bg-gray-200 border border-black rounded-lg flex flex-col items-center justify-center hover:shadow-xl hover:bg-slate-300 transition-all"
             >
               <img
                 src={option.img}
                 alt={option.text}
-                className="h-14 rounded-lg"
+                className="h-12 sm:h-14 rounded-lg"
               />
-              <span className="hover:underline text-gray-800 font-medium hover:text-orange-600 mt-2">
+              <span className="hover:underline text-gray-800 text-sm sm:text-base font-medium hover:text-orange-600 mt-2">
                 {option.text}
               </span>
-            </button>
-          </Link>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
-      <hr />
-      {showModal && <MyModal closeModal={closeModal} type={selectedType} />}
     </div>
   );
-};
-
-export default Programs;
+}
